4abe5e567696b1271e4e3f0909f75e02d235eef7
pedrolosa98 <82504617+pedrolosa98@users.noreply.github.com>
2021-10-14 21:57:00 +0200

    ejercicio-1

---
+++ ejercicio1/js/main.js
@@ -0,0 +1,8 @@
+console.log("Ready :)");
+
+const NombreEl = document.querySelector("#Nombre");
+console.log(NombreEl.value);
+
+const ApellidosLos = document.querySelector("#Apellidos");
+ApellidosLos.classList.add("title");
+console.log(ApellidosLos.value);
